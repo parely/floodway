96b1421a9801b223b7a89973b750ccbc1e07d91a
gundam1001 <s8070015@kmitl.ac.th>
2016-11-26 16:28:23 +0700

    Add files via upload

---
+++ Rubber2553.py
@@ -0,0 +1,23 @@
+import csv
+import plotly.plotly as py
+from plotly.graph_objs import *
+py.sign_in('aul99999', 'YJdLMTY53oqOanHFhxrw')
+
+with open("Rubber.csv", "r") as database:
+    reader = csv.DictReader(database)
+    month = {}
+    num = 1
+    for i in reader:
+        month.setdefault(num, [i["YEAR"], i["2553"]])
+        num += 1
+
+    xReader = []
+    yReader = []
+    for i in sorted(month):
+        xReader.append(month[i][0])
+        yReader.append(int(month[i][1]))
+
+trace1 = Scatter(x = xReader, y = yReader)
+
+data = Data([trace1])
+plot_url = py.plot(data)
