e51bec676b82be19866c6b5ab3d0802494823d8b
gundam1001 <s8070015@kmitl.ac.th>
2016-12-06 20:04:04 +0700

    Add files via upload

---
+++ rice_year.py
@@ -0,0 +1,23 @@
+import csv
+import plotly.plotly as py
+from plotly.graph_objs import *
+py.sign_in('gundam1001', 'wa9h2ohwtx')
+
+with open("rice.csv", "r") as database:
+    reader = csv.DictReader(database)
+    month = {}
+    num = 1
+    for i in reader:
+        month.setdefault(num, [i["YEAR"], i["PRICE"]])
+        num += 1
+
+    xReader = []
+    yReader = []
+    for i in sorted(month):
+        xReader.append(month[i][0])
+        yReader.append(month[i][1])
+
+trace1 = Scatter(x = xReader, y = yReader)
+
+data = Data([trace1])
+plot_url = py.plot(data)
